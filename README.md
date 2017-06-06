6e97aba6b428188c7a5b14e9a87bd7a606be8e74
weiyuan <weiyuan@mac.local>
2017-06-06 20:43:27 +0800

    调整目录

--- README.md
+++
@@ -1,2 +0,0 @@
-# mysqlt
-mysql test
--- main.cpp
+++ main.cpp
@@ -1,59 +1,85 @@
-#include "slave.h"
-#include <string.h>
 
 
+#include "mysqlc.h"
+// #include "thread.h"
 
-void cutstring(char *str,string param[2]) {
-	const char *d = "=";
-	char *p = strtok(str,d);
-	int i=0;
-	while(p) {
-		param[i]=p;
-		p=strtok(NULL,d);
-		i++;
-	}
-}
-
-bool isParam(string str,string param) {
-	if(str == param)
-		return true;
-	else
-		return false;
-}
+#include <pthread.h>
+using namespace std;
+#define NUM_THREADS 5
 
 
-
-int main(int argc, char *argv[])
+void* say_hello(void* args)
 {
-	string ip="127.0.0.1";
-	string port="3306";
-	string user="root";
-	string password="root";
-	string jumpcode="1062";
-	string param[2];
-	for(int i = 1; i < argc; i++) {
-		cutstring(argv[i],param);
-		if(isParam("--ip",param[0])){
-			ip=param[1];
-		}
-		if(isParam("--port",param[0])){
-			port=param[1];
-		}
-		if(isParam("--user",param[0])){
-			user=param[1];
-		}
-		if(isParam("--password",param[0])){
-			password=param[1];
-		}
-		if(isParam("--jumpcode",param[0])){
-			jumpcode=param[1];
-		}
-		if(isParam("--help",param[0])){
-			cout << "./mysql_jump_error [--port=3306] [--ip=127.0.0.1] [--user=root] [--password=root] [--jumpcode=1062]" << endl;
-			return 0;
+	std::cout << "Hello Runoob！" << std::endl;
+}
+
+
+void* insert(void * arg) {
+	MysqlC* mysqlc = new MysqlC("192.168.56.104", 3306, "hotdb_datasource", "hotdb_datasource");
+	mysqlc->use_schema("test");
+	std::cout << "IN" << std::endl;
+	while(1) {
+		if (!mysqlc->execute("insert into t1 value(1)"))
+		{
+			std::cout << "ERROR" << std::endl;
 		}
 	}
-	Slave *slave = new Slave(ip,port,user,password);
-	slave->jumpError(jumpcode);
+}
+
+
+int main(int argc, char const *argv[])
+{
+
+	// insert(NULL);
+	/* code */
+	// MysqlC* mysqlc = new MysqlC("192.168.56.104", 3306, "hotdb_datasource", "hotdb_datasource");
+
+
+// 	mysqlc->use_schema("test");
+// 	sql::ResultSet *res;
+// 	mysqlc->print_log("asd");
+// 	while(1) {
+// 		res = mysqlc->query("show tables");
+
+// 		while (res->next()) {
+// 			std::cout << "\t... MySQL test tables: ";
+// /* Access column data by alias or column name */
+// 			std::cout << res->getString(1) << std::endl;
+// 			std::cout << "\t... MySQL says it again: ";
+// /* Access column data by numeric offset, 1 is the first column */
+// 			std::cout << res->getString(1) << std::endl;
+// 		}
+// 	}
+
+
+// 	// for (int i = 0; i > -1 ; ++i)
+// 	// {
+// 	// 	mysqlc->execute("insert into t1 value(1)") ;
+// 	// }
+
+// 	while(1) {
+// 		if (!mysqlc->execute("insert into t1 value(1)"))
+// 		{
+// 			return 10;
+// 		}
+// 	}
+
+	// pthread_t tids[NUM_THREADS];
+	// for(int i = 0; i < NUM_THREADS; ++i)
+	// {
+ //        //参数依次是：创建的线程id，线程参数，调用的函数，传入的函数参数
+	// 	int ret = pthread_create(&tids[i], NULL, insert, NULL);
+	// 	if (ret != 0)
+	// 	{
+	// 		cout << "pthread_create error: error_code=" << ret << endl;
+	// 	}
+	// }
+ //    //等各个线程退出后，进程才结束，否则进程强制结束了，线程可能还没反应过来；
+	// pthread_exit(NULL);
+
+	Thread* t = new Thread(5,insert);
+	t->run_thread(insert,(void*)NULL);
+	t->run_thread(insert,(void*)NULL);
+	t->run_thread(insert,(void*)NULL);
 	return 0;
 }
